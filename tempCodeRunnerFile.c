4ebb2dbfd32b2628fde54a5238c3780bc5636157
michassm <michelm240703@gmail.com>
2026-05-27 19:46:22 -0300

    init

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*
+    S.C.S app (Sistema de Caixa de Supermercado)
+    SUPER MERCADO WYDEN
+    Participantes: 
+        Michel da Silva Moreira         RA: 202601387642
+        Rute Carboni Moreira            RA: 202601733461
+        Amanda Araujo Sanches           RA: 202602499452
+        Manuelly Tristão de Carvalho    RA: 202602113643
+
+
+*/
+
+void iniciarSistema(){
+    printf("\n Bem vindo ao Super Mercado Wyden");
+    printf("\n Deseja iniciar o atendimento? (s/n)\n");
+}
+
+int main(){
+    
+    iniciarSistema();
+
+    return 0;
+}
